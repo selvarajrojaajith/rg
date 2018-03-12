0dc8ae46002ed5c0b763dcf36a96205d81a6ccd5
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-12 16:07:50 +0530

    maximum.c

---
+++ maximum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,n;
+for(i=0;i<10;i++)
+{
+printf("Enter the number");
+scanf("%d");
+}
+for(j=0;j<10;j++)
+{
+n=a[j];
+}
+printf("The largest number");
+return 0;
+}
